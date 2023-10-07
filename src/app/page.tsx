a66c29d182b63f9a38052d0064288c79f431fb9a
SamawiaShahid <samawiashahid725@gmail.com>
2023-10-07 04:50:41 -0700

    working on contact.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import Header from './components/Header'
 import Body from './components/Body'
+import Experience from './components/Experience'
+import Skills from './components/Skills'
+import Contact from './components/Contact'
 
 export default function Home() {
   return (
     <main className="h-screen overflow-y-scroll bg-gradiant-to-b from-slate-800 to-slate-600">
     <Header/>
     <Body/>
+    <Experience/>
+    <Skills/>
+    <Contact/>
     </main>
   )
 }
